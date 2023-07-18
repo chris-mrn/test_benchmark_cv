2a5b75cc43264fb0e7bc4970b2319d4b36d65cd7
chris-mrn <109912652+chris-mrn@users.noreply.github.com>
2023-07-18 13:44:05 +0200

    simulated dataset

--- datasets/simulated.py
+++ datasets/simulated.py
@@ -17,13 +17,7 @@ class Dataset(BaseDataset):
     # List of parameters to generate the datasets. The benchmark will consider
     # the cross product for each key in the dictionary.
     # Any parameters 'param' defined here is available as `self.param`.
-    parameters = {
-        'n_samples, n_features': [
-            (1000, 500),
-            (5000, 200),
-        ],
-        'random_state': [27],
-    }
+    random_state = 42
 
     def get_data(self):
         # The return arguments of this function are passed as keyword arguments
@@ -31,9 +25,18 @@ class Dataset(BaseDataset):
         # API to pass data. It is customizable for each benchmark.
 
         # Generate pseudorandom data using `numpy`.
-        rng = np.random.RandomState(self.random_state)
-        X = rng.randn(self.n_samples, self.n_features)
-        y = rng.randn(self.n_samples)
+        # making random symmetric matrices
+        random_matrices = []
+        for i in range(576):
+            a = np.random.uniform(0, 1)
+            d = np.random.uniform(0, 1)
+            b = np.random.uniform(-1, 1)
+            random_matrix = np.array([[a, b], [0, d]])
+            random_matrix = np.dot(random_matrix, random_matrix.transpose())
+            random_matrices.append(random_matrix)
+
+        X = np.array(random_matrices)
+        y = np.random.randint(0, 8, 576)
 
         # The dictionary defines the keyword arguments for `Objective.set_data`
         return dict(X=X, y=y)
